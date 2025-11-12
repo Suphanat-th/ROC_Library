31fcbcda3e75135830fb8bde2123a6e4a28d7d99
Bozm <70587200+Suphanat-th@users.noreply.github.com>
2025-11-12 13:12:08 +0700

    Update next.config.ts

--- roc-library/next.config.ts
+++ roc-library/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // <-- ทำให้ Next.js export เป็น static site
-  distDir: "out", // Next.js จะสร้างโฟลเดอร์ static ที่นี่
   reactCompiler: true,
 };
 
