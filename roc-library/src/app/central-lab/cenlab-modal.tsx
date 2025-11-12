6c8cb0dc2cf9bc2d40f75b994278502345ac0093
Bozm <70587200+Suphanat-th@users.noreply.github.com>
2025-11-12 17:13:03 +0700

    update responsive

--- roc-library/src/app/central-lab/cenlab-modal.tsx
+++ roc-library/src/app/central-lab/cenlab-modal.tsx
@@ -18,7 +18,7 @@ const EventModal: React.FC<EventModalProps> = ({ event, onClose }) => {
   if (!event) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-900/90 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-gray-900/90 flex justify-center items-center z-50 overflow-x-auto ">
       <div className="bg-white rounded-lg shadow-lg p-6 w-full min-h-[600px]">
         <h1 className="font-bold text-3xl text-center text-gray-600 mb-4">
           {new Date(event.event_date).toLocaleDateString(undefined, {
@@ -32,7 +32,7 @@ const EventModal: React.FC<EventModalProps> = ({ event, onClose }) => {
           className="font-semibold mb-2 text-black text-3xl text-center mt-10"
           dangerouslySetInnerHTML={{ __html: event.event_title }}
         ></div>
-        <div className="grid grid-cols-9 grid-rows-3 gap-2 w-[900px] h-[500px] rounded-md overflow-hidden mb-4 mx-auto relative">
+        <div className="grid grid-cols-9 grid-rows-3 gap-2 rounded-md overflow-hidden mb-4 mx-auto relative w-[350px] h-[200px] lg:w-[900px] lg:h-[500px] md:w-[750px] md:h-[400px] sm:w-[650px] sm:h-[350px]">
           {/* พื้นหลัง */}
           <div
             className="absolute inset-0 bg-center bg-cover"
