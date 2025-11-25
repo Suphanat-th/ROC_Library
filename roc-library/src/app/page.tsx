58ed1a5ab6a6ed0d96fc399d85231886ce442d92
Bozm <70587200+Suphanat-th@users.noreply.github.com>
2025-11-26 06:41:32 +0700

    update patch

--- roc-library/src/app/page.tsx
+++ roc-library/src/app/page.tsx
@@ -1,9 +1,11 @@
 import ImageSlider from "@/components/ImageSlide/ImageSlide";
+import ServerInfo from "@/components/ServerInfo/ServerInfo";
 
 export default function Home() {
   return (
-    <main className="flex items-center bg-bg text-accent font-8bit pt-[70px] pb-[50px] m-5">
+    <main className="grid items-center bg-bg text-accent font-8bit pt-[70px] pb-[50px] m-5">
       <ImageSlider></ImageSlider>
+      <ServerInfo></ServerInfo>
     </main>
   );
 }
---
+++ roc-library/src/components/ServerInfo/ServerInfo.tsx
@@ -0,0 +1,25 @@
+export default function ServerInfo() {
+  return (
+    <div className="w-full rounded shadow-lg mx-auto my-8">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-600 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-600">
+          #winter
+        </span>
+      </div>
+    </div>
+  );
+}
