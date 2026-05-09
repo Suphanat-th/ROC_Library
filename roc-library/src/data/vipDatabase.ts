07f40d6c64eeb78210c1a79a2c0c0abf78857542
Bozm <70587200+Suphanat-th@users.noreply.github.com>
2026-05-09 20:13:20 +0700

    update error

--- roc-library/src/data/vipDatabase.ts
+++ roc-library/src/data/vipDatabase.ts
@@ -6,20 +6,12 @@ import rawData from "./vipDatabase.json";
  * โครงสร้าง: รหัส, ชื่อ-นามสกุล, ชื่อ Facebook, Link Facebook, เบอร์โทรศัพท์
  */
 export const vipDatabase: VIP[] = [
-  ...rawData.vip.map((item, index) => ({
+  ...rawData.map((item, index) => ({
     id: index + 1,
     code: item.code,
-    fullName: item.name,
+    fullName: item.fullName,
     facebookName: item.facebookName,
-    facebookUrl: item.facebookLink,
-    phoneNumber: item.phone,
-  })),
-  ...rawData.mvp.map((item, index) => ({
-    id: rawData.vip.length + index + 1,
-    code: item.code,
-    fullName: item.name,
-    facebookName: item.facebookName,
-    facebookUrl: item.facebookLink,
-    phoneNumber: item.phone,
+    facebookUrl: item.facebookUrl,
+    phoneNumber: item.phoneNumber,
   })),
 ];
